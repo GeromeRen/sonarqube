25599533612b709b28ae4e3516401cf9fc13cdc8
chunyu.ren <chunyu.ren@accenture.com>
2017-12-22 10:11:56 +0800

    Add jdbc as environment parameters

--- windows-docker-external-azureSQL/sonarqube.cmd
+++ windows-docker-external-azureSQL/sonarqube.cmd
@@ -6,6 +6,7 @@ set _sonarqube_jdbc_password=%2
 set _ms_azuresql_server_name=%3
 set _ms_azuresql_server_port=%4
 set _sonarqube_jdbc_url='jdbc:sqlserver://%_ms_azuresql_server_name%:%_ms_azuresql_server_port%;database=sonar;user=%_sonarqube_jdbc_username%;password=%_sonarqube_jdbc_password%;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30'
+echo "SonarQube JDBC URL: %_sonarqube_jdbc_url%"
 
 set _TMP=
 for /f "delims=" %%a in ('dir /b "C:\sonarqube\extensions\*.*"') do set _TMP=%%a
