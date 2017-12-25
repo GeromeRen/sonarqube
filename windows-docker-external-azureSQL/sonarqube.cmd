c555007ae07b55f36ddd8560142ddc6f78cdd9a2
chunyu.ren <chunyu.ren@accenture.com>
2017-12-25 18:48:39 +0800

    remove debug info

--- windows-docker-external-azureSQL/sonarqube.cmd
+++ windows-docker-external-azureSQL/sonarqube.cmd
@@ -1,8 +1,6 @@
 @Echo off
 Setlocal
 set _folder='C:\sonarqube\extensions\*.*'
-set _sonarqube_jdbc_url='jdbc:sqlserver://%MS_AZURESQL_SERVER_NAME%:%MS_AZURESQL_SERVER_PORT%;database=sonar;user=%SONARQUBE_JDBC_USERNAME%;password=%SONARQUBE_JDBC_PASSWORD%;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30'
-
 set _TMP=
 for /f "delims=" %%a in ('dir /b "C:\sonarqube\extensions\*.*"') do set _TMP=%%a
 echo "Checking Folder: %_folder%"
@@ -13,7 +11,5 @@ IF {%_TMP%}=={} (
 ) ELSE (
     echo 'Data exists, trying to start with your data'
 )
-echo "***********[DEBUG]SONARQUBE_JDBC_URL*****************"
-echo "%_sonarqube_jdbc_url%"
 
 java -jar lib/sonar-application-%SONARQUBE_VERSION%.jar -Dsonar.log.console=true -Dsonar.jdbc.username="%SONARQUBE_JDBC_USERNAME%" -Dsonar.jdbc.password="%SONARQUBE_JDBC_PASSWORD%" -Dsonar.jdbc.url="jdbc:sqlserver://%MS_AZURESQL_SERVER_NAME%:%MS_AZURESQL_SERVER_PORT%;database=sonar;user=%SONARQUBE_JDBC_USERNAME%;password=%SONARQUBE_JDBC_PASSWORD%;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30" -Dsonar.web.javaAdditionalOpts="%SONARQUBE_WEB_JVM_OPTS% -Djava.security.egd=file:/dev/./urandom"
