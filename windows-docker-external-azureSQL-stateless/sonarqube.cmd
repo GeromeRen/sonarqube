b7f454de53925bd67c9aa7cc5438c4e856e245d5
chunyu.ren <chunyu.ren@accenture.com>
2018-01-18 22:45:03 +0800

    Remove temp folder if exists

--- windows-docker-external-azureSQL-stateless/sonarqube.cmd
+++ windows-docker-external-azureSQL-stateless/sonarqube.cmd
@@ -15,9 +15,9 @@ IF {%_TMP%}=={} (
     mkdir %_Azurefile_path%
 	xcopy C:\sonarqube\* %_Azurefile_path% /e /i /h
 ) ELSE (
+	if exist "%_Azurefile_path%\temp" rmdir %_Azurefile_path%\temp /s /q
     echo 'Data exists, trying to start with your data'
 )
-
 CD /D %_Azurefile_path%
 
 java -jar lib/sonar-application-%SONARQUBE_VERSION%.jar -Dsonar.log.console=true -Dsonar.jdbc.username="%SONARQUBE_JDBC_USERNAME%" -Dsonar.jdbc.password="%SONARQUBE_JDBC_PASSWORD%" -Dsonar.jdbc.url="jdbc:sqlserver://%MS_AZURESQL_SERVER_NAME%:%MS_AZURESQL_SERVER_PORT%;database=sonar;user=%SONARQUBE_JDBC_USERNAME%;password=%SONARQUBE_JDBC_PASSWORD%;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30" -Dsonar.web.javaAdditionalOpts="%SONARQUBE_WEB_JVM_OPTS% -Djava.security.egd=file:/dev/./urandom"
