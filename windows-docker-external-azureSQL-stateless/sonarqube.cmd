80986080689c1264d0517eecd6842c191ed15774
chunyu.ren <chunyu.ren@accenture.com>
2018-01-18 22:08:03 +0800

    introduce Azure File

--- windows-docker-external-azureSQL-stateless/sonarqube.cmd
+++ windows-docker-external-azureSQL-stateless/sonarqube.cmd
@@ -1,28 +1,23 @@
 @Echo off
 Setlocal
-set _folder='S:\sonarqube\*.*'
 set _TMP=
-set sdrive=S:
-set ssonarqubedrive=S:\sonarqube
+set _Azurefile_path=%AZUREFILE_DRIVE_LETTER%:%AZUREFILE_FOLDER%
 
-echo "[Debug]SONARQUBE_JDBC_USERNAME is: %SONARQUBE_JDBC_USERNAME%"
-echo "[Debug]SONARQUBE_JDBC_USERNAME is: %SONARQUBE_JDBC_USERNAME"
+net use %AZUREFILE_DRIVE_LETTER%: \\%AZUREFILE_URL% /u:AZURE\%AZUREFILE_ACC_NAME% %AZUREFILE_ACC_KEY%
 
+for /f "delims=" %%a in ('dir /b "%_Azurefile_path%\*.*"') do set _TMP=%%a
+echo "Checking Folder: %_Azurefile_path%\*.*"
 
-net use S: \\azurefiledriverac1.file.core.windows.net\sonarqube4windows1 /u:AZURE\azurefiledriverac1 kZaxd8C98Bhjf3HO3YyDJpmBCrP/M5HbO4m/lg0oMqGnYXGBWqtKSNs7O7Rv8kztSgPT05hEbDoMxezFofyUXQ==
-for /f "delims=" %%a in ('dir /b "S:\sonarqube\*.*"') do set _TMP=%%a
-echo "Checking Folder: %_folder%"
-CD /D %sdrive%
-
-if not exist "S:\sonarqube\" mkdir S:\sonarqube
+CD /D %AZUREFILE_DRIVE_LETTER%
 
 IF {%_TMP%}=={} (
-    echo 'First run'
-	xcopy C:\sonarqube\* S:\sonarqube /e /i /h
+    echo 'First run and create %_Azurefile_path%'
+    mkdir %_Azurefile_path%
+	xcopy C:\sonarqube\* %_Azurefile_path% /e /i /h
 ) ELSE (
     echo 'Data exists, trying to start with your data'
 )
 
-CD /D %ssonarqubedrive%
+CD /D %_Azurefile_path%
 
-java -jar lib/sonar-application-%SONARQUBE_VERSION%.jar -Dsonar.log.console=true -Dsonar.jdbc.username="chunyu.ren@sonarserver3" -Dsonar.jdbc.password="Aaaaaaa123!" -Dsonar.jdbc.url="jdbc:sqlserver://sonarserver3.database.windows.net:1433;database=sonar;user=chunyu.ren@sonarserver3;password=Aaaaaaa123!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30" -Dsonar.web.javaAdditionalOpts="%SONARQUBE_WEB_JVM_OPTS% -Djava.security.egd=file:/dev/./urandom"
+java -jar lib/sonar-application-%SONARQUBE_VERSION%.jar -Dsonar.log.console=true -Dsonar.jdbc.username="%SONARQUBE_JDBC_USERNAME%" -Dsonar.jdbc.password="%SONARQUBE_JDBC_PASSWORD%" -Dsonar.jdbc.url="jdbc:sqlserver://%MS_AZURESQL_SERVER_NAME%:%MS_AZURESQL_SERVER_PORT%;database=sonar;user=%SONARQUBE_JDBC_USERNAME%;password=%SONARQUBE_JDBC_PASSWORD%;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30" -Dsonar.web.javaAdditionalOpts="%SONARQUBE_WEB_JVM_OPTS% -Djava.security.egd=file:/dev/./urandom"
